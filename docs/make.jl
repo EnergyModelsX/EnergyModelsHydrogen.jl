5a57e90852d9234d5a6f79f4fdc002a35be19def
Avinash Subramanian <Avinash.Subramanian@sintef.no>
2022-07-19 20:10:07 +0000

    Added documentation functionality from DocumenterTools.jl

---
+++ docs/make.jl
@@ -0,0 +1,37 @@
+push!(LOAD_PATH,"../src/")
+
+try
+    using Documenter
+catch
+    import Pkg
+    Pkg.activate(@__DIR__)
+    Pkg.instantiate()
+    
+    using Documenter
+end
+using EnergyModelsHydrogen
+
+makedocs(
+    sitename = "EnergyModelsHydrogen",
+    format = Documenter.HTML(),
+    modules = [EnergyModelsHydrogen],
+    pages = [
+        "Home" => "index.md",
+        "Manual" => Any[
+            "Quick Start" => "manual/quick-start.md",
+            "Philosophy" => "manual/philosophy.md",
+            "Example" => "manual/simple-example.md",
+        ],
+        "Library" => Any[
+            "Public" => "library/public.md",
+            "Internals" => "library/internals.md"
+        ]
+    ]
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+#=deploydocs(
+    repo = "<repository url>"
+)=#
