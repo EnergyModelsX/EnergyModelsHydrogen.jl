7b7b48a13d0b894f0df078a000550d773b973c15
Avinash Subramanian <Avinash.Subramanian@sintef.no>
2022-09-25 16:22:42 +0000

    Cleaned up tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,7 @@
 using Revise
 using Test
 using EnergyModelsHydrogen
-using GLPK
-using JuMP
 
-#=
-m, case = Hydrogen.run_model("",GLPK.Optimizer)
-println(objective_value(m))
-value.(m[:cap_use])
-
-@testset "Hydrogen module electrolyzer" begin
-    println(objective_value(m))
-    @test 0 == 0
-    # Write your tests here.in
+@testset "Wind Turbine -> Electrolyzer -> H2-consumer" begin
+    include("test_electrolyzer_degradation.jl")
 end
-=#
